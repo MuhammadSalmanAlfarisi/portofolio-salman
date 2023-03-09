0e71de6f89b2c48c095ce691634d7a4eb199ae7f
salman261002 <116475964+MuhammadSalmanAlfarisi@users.noreply.github.com>
2023-03-09 14:23:11 +0700

    fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Cards from "../components/Cards";
 
 const Home = () => {
   return (
-    <div className="flex lg:border-r-gray-600 lg:border-r-4  lg:border-dashed bg-[#f9f9f9] scale-20 lg:scale-100 lg:p-3 p-0 m-0 lg:w-auto  justify-center items-center justify-items-center lg:grow lg:w-[1200px]  w-full">
+    <div className="flex lg:border-r-gray-600 lg:border-r-4  lg:border-dashed bg-[#f9f9f9] scale-20 lg:scale-100 lg:p-3 p-0 m-0 lg:w-auto  justify-center items-center justify-items-center lg:grow  w-full">
       <div className="flex justify-center items-center justify-items-center w-[70%] align-middle content-center">
         <Cards />
       </div>
